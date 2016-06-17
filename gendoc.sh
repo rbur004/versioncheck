bf6f10fc3ed4cb0552f3445fb262c0de865ada7f
robertburrowes <r.burrowes@auckland.ac.nz>
2016-06-17 21:49:33 +1200

    Changed to have git docs as subdirectory, not ../doc

--- gendoc.sh
+++ gendoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #create doc
 /usr/local/bin/rake docs
-cp -r doc/* ../doc
+cp -r doc/* gitdoc
 rm -rf doc
-( cd ../doc; git add . ; git commit -a --allow-empty-message -m ""; git push origin gh-pages )
+( cd gitdoc; git add . ; git commit -a --allow-empty-message -m ""; git push origin gh-pages )
