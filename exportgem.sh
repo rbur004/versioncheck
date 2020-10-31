fd83198c97cf8ea24e572509962364d17c39c56d
robertburrowes <r.burrowes@auckland.ac.nz>
2020-10-31 16:15:11 +1300

    Mv dev scripts to sbin

--- exportgem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Copy up to rubygem.org
-VERSION="1.0.0"
-git tag -a ${VERSION} -m "Gem release ${VERSION}"
-/usr/local/bin/rake release VERSION=${VERSION} #--trace
--- gendoc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#create doc
-/usr/local/bin/rake docs
-cp -r doc/* gitdoc
-rm -rf doc
-( cd gitdoc; git add . ; git commit -a --allow-empty-message -m ""; git push origin gh-pages )
